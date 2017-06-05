66a330cd5d971c402fc1766e12794076501922e5
juan <garcia.solutions@gmail.com>
2017-06-05 12:15:20 -0500

    settings changes

--- src/app/pages/settings/settings.component.ts
+++ src/app/pages/settings/settings.component.ts
@@ -11,25 +11,31 @@ import 'rxjs/Rx';
 
   selector: 'az-inputs',
   encapsulation: ViewEncapsulation.None,
-  templateUrl: './settings.component.html'
+  templateUrl: './settings.component.html',
+  styleUrls:['./settings.component.scss']
 
 
 })
 export class SettingsComponent{
   disabled:boolean = true;
   public settingsForm:FormGroup;
+  public addressForm:FormGroup;
   public userSession:any;
   public setting:any;
+  public addresses = [];
+  
 
   constructor(private fromBuilder:FormBuilder,public http:Http,public local:UserSessionService){
         this.loadSettings();
         this.settingsForm = this.fromBuilder.group({
             'iva':['',Validators.compose([Validators.required])],
             'connectionTime':['',Validators.compose([Validators.required])],
-            'maxAttached':[''],
-            'idSchedule':[''],
-            'idMacs':['']
+            'maxAttached':['',Validators.compose([Validators.required])]
 
+        });
+        this.addressForm = this.fromBuilder.group({
+          'name':['',Validators.compose([Validators.required])],
+          'mac':['',Validators.compose([Validators.required])],
         })
 
         this.userSession = this.local.getUser();
@@ -55,6 +61,28 @@ export class SettingsComponent{
           console.log(this.setting);
           
     })  
+  }
+  saveSettings(){
+      let request = {
+          idMacs: this.addresses
+      }
+      Object.assign(request,this.settingsForm.value);
+      console.log(request);
+      this.http.post(config.url+'setting/add?access_token='+this.local.getUser().token,request).toPromise().then(result=>{
+          console.log(result.json());
+          
+      })
+      
+  }
+  addAddress(){
+    let addFormat = {
+          mac : this.addressForm.value.mac,
+          name: this.addressForm.value.name
+       }
+
+       this.addresses.push(addFormat);
+
+       this.addressForm.setValue({name:'',mac:''});
   }
 
 }
